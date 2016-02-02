7ae2384ea791a6295f1acbe05db9bf1f15402d32
George Holevas <gholevas1@gmail.com>
2016-02-02 16:42:11 -0500

    merged stuff

--- public/js/index.js
+++ public/js/index.js
@@ -1,25 +1,56 @@
- function initialize_gmaps() {
-        // initialize new google maps LatLng object
-        var myLatlng = new google.maps.LatLng(40.705189, -74.009209);
-        // set the map options hash
-        var mapOptions = {
-            center: myLatlng,
-            zoom: 16,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-        // get the maps div's HTML obj
-        var map_canvas_obj = document.getElementById("map-canvas");
-        // initialize a new Google Map with the options
-        var map = new google.maps.Map(map_canvas_obj, mapOptions);
-        // Add the marker to the map
-        var marker = new google.maps.Marker({
-            position: myLatlng,
-            title: "Hello World!"
-        });
-        // Add the marker to the map by calling setMap()
-        marker.setMap(map);
+function initialize_gmaps() {
+
+  // Create an array of styles.
+  var styles = [
+    {
+      stylers: [
+        { hue: "#00ffe6" },
+        { saturation: -20 }
+      ]
+    },{
+      featureType: "road",
+      elementType: "geometry",
+      stylers: [
+        { lightness: 100 },
+        { visibility: "simplified" }
+      ]
+    },{
+      featureType: "road",
+      elementType: "labels",
+      stylers: [
+        { visibility: "off" }
+      ]
     }
+  ];
+
+  // Create a new StyledMapType object, passing it the array of styles,
+  // as well as the name to be displayed on the map type control.
+  var styledMap = new google.maps.StyledMapType(styles,
+    {name: "Styled Map"});
+
+  // Create a map object, and include the MapTypeId to add
+  // to the map type control.
+  var mapOptions = {
+    zoom: 16,
+    center: new google.maps.LatLng(40.705189, -74.009209),
+    mapTypeControlOptions: {
+      mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'map_style']
+    }
+  };
+  var map = new google.maps.Map(document.getElementById('map-canvas'),
+    mapOptions);
+
+  //Associate the styled map with the MapTypeId and set it to display.
+  map.mapTypes.set('map_style', styledMap);
+  map.setMapTypeId('map_style');
+}
+
 
     $(document).ready(function() {
         initialize_gmaps();
+        $('#fsModal').show();
+
     });
+        $('#startTrip').click(function(){
+            $('#fsModal').hide();
+        })
